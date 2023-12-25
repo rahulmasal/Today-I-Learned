641a24dbd3611fbbe8591fa774bfd86aabce76a6
rahulmasal <rahulmasal2013@gmail.com>
2023-12-25 07:38:42 +0530

    test

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
+import {supabaseKey,supabaseUrl}  from './apikey';
 
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFya3Z3Y3lhZ2FlZHZub2FrenB2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDMxMjIxMTMsImV4cCI6MjAxODY5ODExM30.HS3Tgj11RriL_D3MrqaXzuonmQR5ZDXM9hMocb7JKEM";
-const supabaseUrl = "https://qrkvwcyagaedvnoakzpv.supabase.co";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
