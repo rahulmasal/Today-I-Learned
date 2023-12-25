4271de8c1ee64ad0a5c2095af7e07c255b484818
rahulmasal <rahulmasal2013@gmail.com>
2023-12-25 07:50:42 +0530

    keyhide

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-import {supabaseKey,supabaseUrl}  from './apikey';
-
-
+// import {supabaseKey,supabaseUrl}  from './apikey';
+const supabaseUrl=process.env.REACT_APP_URL;
+const supabaseKey=process.env.REACT_APP_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
